fb703090920d7daa2c17ac8ef4b636086cb87cb7
Brendon Costa <brendoncosta93@gmail.com>
2023-10-29 19:25:29 -0300

    refactor in phoneValidator

--- src/shared/validators/phoneValidator.ts
+++ src/shared/validators/phoneValidator.ts
@@ -1,10 +1,12 @@
 import {z} from "zod";
+import {removeSpecialCharacters} from "../utils/remove-special-characters.ts";
 
 export const phoneValidator = () => {
     return z.string().refine(value => {
-        if (value.trim() === '') return false;
-        const phoneRegex = /^\(\d{2}\) \d{5}-\d{4}$/;
-        return phoneRegex.test(value);
+        if (!value) return false;
+        value = removeSpecialCharacters(value);
+        return value.length >= 10;
+
     }, {
         message: 'Telefone inválido',
     });
