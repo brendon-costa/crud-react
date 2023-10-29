90fe85a991e6e80e57c177968ce6f574f66cb71f
Brendon Costa <brendoncosta93@gmail.com>
2023-10-29 16:37:26 -0300

    implement repository in localStorage

--- src/pages/register/services/register.service.tsx
+++ src/pages/register/services/register.service.tsx
@@ -2,18 +2,18 @@ import {AxiosResponse} from "axios";
 import api from "../../../core/axios/api.ts";
 import {UserModel} from "../model/user.model.ts";
 
-const repositoriKey = 'registerList';
+const repositoryKey = 'registerList';
 
 export const getRegisterListInApi = (): Promise<AxiosResponse<UserModel[]>> => {
     return api.get(`/users`);
 }
 
 export const getRegisterListInStorage = (): UserModel[] => {
-    const registerList = localStorage.getItem(repositoriKey);
+    const registerList = localStorage.getItem(repositoryKey);
     return registerList ? JSON.parse(registerList) : [];
 
 }
 
 export const setRegisterListInStorage = (registerList: UserModel[]): void => {
-    localStorage.setItem(repositoriKey, JSON.stringify(registerList));
+    localStorage.setItem(repositoryKey, JSON.stringify(registerList));
 }
